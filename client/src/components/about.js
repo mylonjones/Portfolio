43110f21d0ba91f60c293424e48732c781b0fe54
mylonjones <mylonndj1234@gmail.com>
2021-07-02 12:50:07 -0700

    added connect 4 game

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -4,17 +4,17 @@ export default function About () {
   return(<div className='about'>
 
     <p>
-      <img className='selfie' src='/selfie.jpg' width='200'/>
+      <img className='selfie' src='./selfie.jpg' width='200'/>
       <p>
-        My name is Mylon Jones. I am a Software Engineer specializing in full-stack web development. I like software engineering because of the innumerable uses which can, and do, improve life in countless ways. I want to use my software engineering skills to make the world more interactive.
+        My name is Mylon Jones. I am a Software Engineer specializing in full-stack web development. I became a software engineering because of the countless ways it can improve the world. I want to use my software engineering skills to make the world more interactive.
       </p>
 
       <p>
-        I am experienced and comfortable with working alone, pair-programming, and team collaboration. Among my various other projects, I recently collaborated with a team of four engineers to design and build a full-stack web application that keeps track of vehicle maintenance using React, Express, and MongoDB. My philosophy is to do my job as if I were doing it for myself or do it better. I don’t just stop when I finish the basic tasks, I look to see if there is anything more I can do.
+        I am experienced and work comfortably alone, pair-programming, and in team collaboration. Among my various projects, I recently collaborated with a team of four engineers to design and build a full-stack web application that keeps track of vehicle maintenance using React, Express, and MongoDB. My philosophy is to do my job as well as I can, doing whatever it takes. I like to do as much as I can on my own, but when I reach a blocker I cannot overcome, I will reach out for guidance. I don’t just stop when I finish the basic tasks. I look to see if there is anything more that I can do.
       </p>
 
       <p>
-        I aim to find a workplace where I can use creativity and problem solving to build interactive tools and applications that are ethical, valuable, and diverse. I would be in a dynamic team of results-driven engineers whom I could hang out with outside of work on occasion. It would also be ideal to someday be a mentor because I want to be helpful to my fellow engineers as much as I can.
+        I aim to find a workplace where I can use creativity and problem solving to build interactive tools and applications that are ethical, valuable, and diverse. I would be in a dynamic team of results-driven engineers whom I could hang out with outside of work on occasion. It would also be ideal to someday be a mentor because I want to be as helpful to my fellow engineers as I can.
       </p>
 
       <p>
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Home from './home.js'
 import About from './about.js'
 import Contact from './contact.js'
+import Board from './board.js'
 
 export default function Navbar(props) {
   return (<div className='navbar'>
@@ -18,6 +19,7 @@ export default function Navbar(props) {
       <div className='link' onClick={()=> {props.changePage(<About/>)}}>About</div>
       <a className='link' target='blank' href="./Mylon Jones Resume.pdf">Resume</a>
       <div className='link' onClick={()=> {props.changePage(<Contact/>)}}>Contact</div>
+      <div className='link' onClick={()=> {props.changePage(<Board/>)}}>Game</div>
     </div>
   </div>)
 }
