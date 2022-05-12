779f9c150a41563b220b83806ba838a69055669c
mylonjones <mylonndj1234@gmail.com>
2022-05-12 11:27:32 -0700

    made a clock

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from "react-router-dom";
 
+
 export default function Home (props) {
+
+
+  const getTime = function() {
+    const time = new Date
+    let meridian = ' am'
+    let hours = time.getHours()
+    if(hours > 11) meridian = ' pm'
+    if(hours > 12) hours -= 12
+    if(hours < 10) hours = '0' + hours
+    let minutes = time.getMinutes()
+    if(minutes < 10) minutes = '0' + minutes
+    let seconds = time.getSeconds()
+    if(seconds < 10) seconds = '0' + seconds
+    let clock = document.getElementById('clock')
+    clock.innerHTML = (hours + ':' + minutes + ':' + seconds + meridian)
+    setTimeout(getTime, 1000)
+  }
+
+  useEffect(getTime)
+
   return(<div className='homepage'>
     <div className='name'>
       Mylon Jones
@@ -17,6 +38,8 @@ export default function Home (props) {
     </div>
     <div className='portfolioButtons'>
       <Link className='project' to='Q&A'>{"Q&A Model"}</Link>
+      <div id='clock'>
+      </div>
       <Link className='project' to="/Connect4">Connect 4</Link>
     </div>
   </div>)
